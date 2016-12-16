773f54c39e27d7389adf9ed5ff8df1faea2db2e7
cy2003 <catyyee@hotmail.com>
2016-12-16 14:49:30 -0500

    Done

--- app.rb
+++ app.rb
@@ -13,5 +13,22 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+  get '/goodbye/:name' do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}"
+  end
+
+  get '/multiply/:num1/:num2' do
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    #num1 and num2 does not need to be an instance variable
+    #You only need an instance variable if you're going to show
+    #it to the viewer
+    @result = num1 * num2
+    #because result is going to be shown to the viewer it needs
+    #to be an instance variable
+    "#{@result}"
+  end
+
 
 end
